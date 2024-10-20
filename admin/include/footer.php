182cc09d47f1d618d1ce51e96fa2566667d3ba66
HuuThang <huuthangblue0512@gmail.com>
2024-10-20 15:38:28 +0700

    first commit

---
+++ admin/include/footer.php
@@ -0,0 +1,9 @@
+	<div class="footer">
+	    <div class="container">
+
+
+	        <b class="copyright">&copy; 2024 <a href="https://www.facebook.com/huuthang.thaipham.90">ThaiPhamHuuThang</a>
+	        </b> All rights
+	        reserved.
+	    </div>
+	</div>
